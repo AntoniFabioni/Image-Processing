38c3927f109845e8485415057213c0024a28f838
Anthony Fabian <anthony.fabian88@gmail.com>
2020-09-14 11:01:46 -0400

    Update Smart-Scale.py

--- Smart-Scale.py
+++ Smart-Scale.py
@@ -1,7 +1,7 @@
 '''
 This program implements "smart image scaling" using a seam carving algorithm.
 The image will be scaled such that its width will be reduced while preserving
-important details.
+important details by removing low-contour regions.
 '''
 
 import sys
