c4741df6562293154c91d0c52f4260f90ee58ec2
Anthony Fabian <anthony.fabian88@gmail.com>
2020-10-19 13:18:25 -0400

    Update Distort.py

--- Distort.py
+++ Distort.py
@@ -1,11 +1,25 @@
-# Work in progress. Plan is to distort an image in a cool way, maybe using fractals??
+# Work in progress. Plan is to distort an image in a cool way.
 
+# import numpy as np
+# import sys
+# import numba
+# from imageio import imread, imwrite
+# from scipy.ndimage.filters import convolve
+# from tqdm import trange
+
+import scipy.misc
 import numpy as np
-import sys
-import numba
-from imageio import imread, imwrite
-from scipy.ndimage.filters import convolve
-from tqdm import trange
+import matplotlib.pyplot as plt
 
-def F(x, y):
-    print("F")
+img = scipy.misc.lena()
+
+A = img.shape[0] / 3.0
+w = 2.0 / img.shape[1]
+
+shift = lambda x: A * np.sin(2.0*np.pi*x * w)
+
+for i in range(img.shape[0]):
+    img[:,i] = np.roll(img[:,i], int(shift(i)))
+
+plt.imshow(img, cmap=plt.cm.gray)
+plt.show()
