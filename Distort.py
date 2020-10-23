97e3f85eaf5ce447a0f4ce5d3156728953213541
Anthony Fabian <anthony.fabian88@gmail.com>
2020-10-23 16:54:40 -0400

    Update Distort.py

--- Distort.py
+++ Distort.py
@@ -17,7 +17,7 @@ img = scipy.misc.lena() # Error??
 A = img.shape[0] / 3.0
 w = 2.0 / img.shape[1]
 
-shift = lambda x: A * np.sin(2.0*np.pi*x * w)
+shift = lambda x: A * np.sin(2.5*np.pi*x * w)
 
 for i in range(img.shape[0]):
     img[:,i] = np.roll(img[:,i], int(shift(i)))
