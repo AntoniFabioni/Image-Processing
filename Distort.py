48fe650f99ec0fdef18bde631a424ef11aacdeda
Anthony Fabian <anthony.fabian88@gmail.com>
2020-11-09 18:09:40 -0500

    Update Distort.py

--- Distort.py
+++ Distort.py
@@ -19,7 +19,9 @@ img = scipy.misc.lena() # Error...no attribute 'lena'??
 A = img.shape[0] / 3.0
 w = 2.0 / img.shape[1]
 
-shift = lambda x: A * np.sin(4*np.pi*x * w)
+k = 4
+
+shift = lambda x: A * np.sin(k*np.pi*x * w)
 
 for i in range(img.shape[0]):
     img[:,i] = np.roll(img[:,i], int(shift(i)))
