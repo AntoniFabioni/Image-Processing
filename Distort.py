7d2859bea169f6721c3bc993810f4ec98e7da07c
Anthony Fabian <anthony.fabian88@gmail.com>
2020-11-19 18:23:41 -0500

    Update Distort.py

--- Distort.py
+++ Distort.py
@@ -23,7 +23,7 @@ img = scipy.misc.lena() # Error...no attribute 'lena'??
 A = img.shape[0] / 3.0
 w = 2.0 / img.shape[1]
 
-k = 4
+k = 4 # Make variable when calling?
 
 shift = lambda x: A * np.sin(k*np.pi*x * w)
 
